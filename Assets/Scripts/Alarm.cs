97f02649fd2b95c8ba37d92ec37cda42da6bcb7e
l1l6dusa <63962252+l1l6dusa@users.noreply.github.com>
2020-10-01 12:57:26 +0300

    Thief&Alarm complete study
    thief&alarm completed version

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+
+    [SerializeField] private float _alarmVolumeSwingPeriod;
+    [Range(0.1f, 1f)][SerializeField] private float _maxVolume;
+    [Range(0.1f, 1f)][SerializeField] private float _minVolume;
+    private AudioSource audio;
+    private Coroutine alarm;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.volume = _maxVolume;
+    }
+    
+    void Update() { }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Thief>(out var thief))
+        {
+            alarm = StartCoroutine(ChangeVolumeCoroutine(_alarmVolumeSwingPeriod));
+            
+            audio.Play();
+        }
+        
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        
+        if (other.TryGetComponent<Thief>(out var thief))
+        { 
+            StopCoroutine(alarm);
+            
+            audio.Stop();
+        }
+
+       
+    }
+
+    private IEnumerator ChangeVolumeCoroutine(float time)
+    {
+       
+        var isSwingMiddlePassed = false;
+        float timer = 0;
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if (isSwingMiddlePassed == false)
+            {
+                if (timer < time)
+                {
+                    audio.volume = Mathf.Clamp(audio.volume - Time.deltaTime * (_maxVolume - _minVolume), _minVolume,
+                        _maxVolume);
+                    yield return new WaitForEndOfFrame();
+                }
+
+                if (audio.volume == _minVolume)
+                {
+                    isSwingMiddlePassed = true;
+                    timer = 0;
+                }
+            }
+            else
+            {
+                timer += Time.deltaTime;
+                if (isSwingMiddlePassed == true)
+                {
+                    if (timer < time)
+                    {
+                        audio.volume = Mathf.Clamp(audio.volume + Time.deltaTime * (_maxVolume - _minVolume), _minVolume,
+                            _maxVolume);
+                        yield return new WaitForEndOfFrame();
+                    }
+
+                    if (audio.volume == _maxVolume)
+                    {
+                        isSwingMiddlePassed = false;
+                        timer = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : MonoBehaviour
+{
+    [SerializeField] private int _speed;
+    private Rigidbody2D rbody;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rbody.MovePosition(gameObject.transform.position + Vector3.left * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rbody.MovePosition(gameObject.transform.position + Vector3.right * _speed * Time.deltaTime);
+        }
+    }
+}
